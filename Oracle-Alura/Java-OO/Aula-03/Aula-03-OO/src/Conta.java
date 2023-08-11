53a5c6f679ce1c2f67fff7e19c1dfa7aa0bb6556
Rafael Cerqueira <rafaelcerqueiraf@outlook.com>
2023-08-11 12:24:40 -0300

    Orientacao a Objetos - Metodos com Referencias

---
+++ Oracle-Alura/Java-OO/Aula-03/Aula-03-OO/src/Conta.java
@@ -0,0 +1,45 @@
+public class Conta {
+    double saldo;
+    int agencia;
+    int numero;
+    String titular;
+
+    public void deposita(double valor) {
+      this.saldo += valor;
+      if (valor > 0) {
+          System.out.println("Depósito feito com sucesso.");
+      } else {
+          System.out.println("Erro ao fazer depósito");
+      }
+
+      System.out.println("Seu saldo é de R$ " + saldo);
+    };
+
+    public void saca(double valor) {
+        if (this.saldo > 0) {
+            if (valor > this.saldo) {
+                System.out.println("Valor maior do que o saldo.");
+            } else if (valor <= this.saldo) {
+                this.saldo = this.saldo - valor;
+
+                System.out.println("Saque feito com sucesso!");
+                System.out.println("Seu novo saldo é R$ " + this.saldo);
+            }
+        } else {
+            System.out.println("Saldo insuficiente! Deposite algum valor na conta.");
+        }
+    }
+
+    public void transfere(double valor, Conta destino) {
+        if (this.saldo > 0) {
+            if (valor <= this.saldo) {
+                this.saldo -= valor;
+                System.out.println("Transferência realizada com sucesso! Seu novo saldo é de R$ " + this.saldo);
+            } else {
+                System.out.println("Valor maior do que o saldo!");
+            }
+        } else {
+            System.out.println("Saldo insuficiente! Deposite algum valor na conta.");
+        }
+    }
+}
