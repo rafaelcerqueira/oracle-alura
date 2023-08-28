3f40efb4a440ca9c2feff9d827728932b9cc2682
Rafael <rafaelcerqueiraf@outlook.com>
2023-08-28 18:16:50 -0300

    Testando os Getters e Setters.

--- Aula-03-getter-setters/Getters-Setters/src/TestaGetESet.java
+++ Aula-03-getter-setters/Getters-Setters/src/TestaGetESet.java
@@ -2,7 +2,9 @@ public class TestaGetESet {
     public static void main(String[] args) {
         Conta conta = new Conta();
         conta.setNumero(2023);
+        System.out.println(conta.getNumero());
 
+        conta.setNumero(2024);
         System.out.println(conta.getNumero());
     }
 }
